d89f57833534acd109802386a68778abf8889e8e
deondreE <166420956+EnglishDeondre-FS@users.noreply.github.com>
2024-05-03 01:09:22 -0500

    created a simple logger for the project

--- include/core.h
+++ include/core.h
@@ -2,14 +2,16 @@
 
 // core file for general std libriries / export macros.
 #include <stdio.h>
+#include <stdarg.h> 
+#include <stdbool.h>
 #include <string.h>
+#include <time.h>
 #include <sys/stat.h>
 
-
 // File Handle Struct
-typedef struct {
-    FILE *file;
+// typedef struct {
+//     FILE *file;
 
-    int is_dir; // could have files inside of it. 
-} FileHandle;
+//     int is_dir; // could have files inside of it. 
+// } FileHandle;
 
