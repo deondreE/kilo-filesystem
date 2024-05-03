75be7843aac73240aa80d189e0d92ffb83fcab00
deondreE <166420956+EnglishDeondre-FS@users.noreply.github.com>
2024-05-03 10:58:26 -0500

    tested the logger it works!; added launch scripts don't work :(.

--- tests/main.c
+++ tests/main.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
+#include <log.h>
 // I will have to use this file to create all kinds of tests,
 // and use cases for the core of the project.
 int main()
 {
+  log_trace("test");
   printf("testing this test project!");
   return 0;
 }
--- todo.rst
+++ todo.rst
@@ -1,4 +1,3 @@
-1. Export Macros, for all platform the app is going to launch on.
 2. Launch Scripts for vscode.
 3. Swift App connecting to the C backend.
 4. Start directory indexing.
